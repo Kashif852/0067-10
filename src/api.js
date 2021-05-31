c7ade197dd122b254f0d22bf74754004b4df71bc
Kashif_Hussain <49343689+Kashif852@users.noreply.github.com>
2021-05-31 14:27:38 +0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -12,5 +12,29 @@ router.get("/", (req, res) => {
 
 app.use(`/.netlify/functions/api`, router);
 
+// Imagekit
+const ImageKit = require('imagekit');
+
+
+const imagekit = new ImageKit({
+  publicKey : "public_aEaOaYimggRM+4kgwtcDb4PKl9Y=",
+    privateKey : "private_D7+4Zno0EkO2fdFhMwTeX8Zwpho=",
+    urlEndpoint : "https://ik.imagekit.io/ymjlyrr8zyb"
+});
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", 
+      "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+  router.get('/auth', function (req, res) {
+    var result = imagekit.getAuthenticationParameters();
+    res.send(result);
+  });
+
+
 module.exports = app;
 module.exports.handler = serverless(app);
